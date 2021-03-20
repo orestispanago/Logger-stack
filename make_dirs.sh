449877311238338baca35c674de95fca13478c42
orestispanago <orestispanago@gmail.com>
2021-03-20 16:29:24 +0200

    Named containers, added sql init script

--- make_dirs.sh
+++ make_dirs.sh
@@ -1,7 +1,10 @@
-GRAFANA="./volumes/grafana"
 MYSQL="./volumes/mysql"
+GRAFANA="./volumes/grafana"
 NODERED="./volumes/node-red"
 
+mkdir -p $MYSQL/data
+mkdir -p $MYSQL/init
 mkdir -p $GRAFANA
-mkdir -p $MYSQL
 mkdir -p $NODERED
+
+cp init.sql $MYSQL/init
